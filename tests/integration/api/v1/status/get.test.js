99dc0c1448744fad811d641ea28552246d5bfecf
mendes-iago <oiagomendes@gmail.com>
2025-03-14 23:30:50 +0000

    finish `api/v1/status` endpoint

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,20 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  expect(responseBody.dependencies.database.version).toBe("16.0");
+  expect(responseBody.dependencies.database.max_connections).toBe(100);
+  expect(responseBody.dependencies.database.opened_connections).toBe(1);
+
+  // const maxConnections = responseBody.max_connections;
+  // expect(maxConnections).toBeDefined();
+
+  // const usedConnections = responseBody.used_connections;
+  // expect(usedConnections).toBeDefined();
+  // expect(usedConnections).toBe(1);
 });
